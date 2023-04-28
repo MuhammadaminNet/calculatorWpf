ab485bfe9b21685a58db7fa7599d6a0cbec07e53
MuhammadaminNet <mburanbayevdev@gmail.com>
2023-04-28 19:51:27 +0500

    calculator

---
+++ WpfCalculatorApp/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+
+namespace WpfCalculatorApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly RequaredMethods requaredMethods;
+        internal string tmpSaver = "0";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            requaredMethods = new RequaredMethods(this);
+        }
+
+        private void ACbtn_Click(object sender, RoutedEventArgs e) => (MainInput2.Text, tmpSaver) = ("0", "0");
+        private void b9_Click(object sender, RoutedEventArgs e) => requaredMethods.NumberBtnClick('9');
+        private void b8_Click(object sender, RoutedEventArgs e) => requaredMethods.NumberBtnClick('8');
+        private void b7_Click(object sender, RoutedEventArgs e) => requaredMethods.NumberBtnClick('7');
+        private void b6_Click(object sender, RoutedEventArgs e) => requaredMethods.NumberBtnClick('6');
+        private void b5_Click(object sender, RoutedEventArgs e) => requaredMethods.NumberBtnClick('5');
+        private void b4_Click(object sender, RoutedEventArgs e) => requaredMethods.NumberBtnClick('4');
+        private void b3_Click(object sender, RoutedEventArgs e) => requaredMethods.NumberBtnClick('3');
+        private void b2_Click(object sender, RoutedEventArgs e) => requaredMethods.NumberBtnClick('2');
+        private void b1_Click(object sender, RoutedEventArgs e) => requaredMethods.NumberBtnClick('1');
+        private void b0_Click(object sender, RoutedEventArgs e) => requaredMethods.NumberBtnClick('0');
+        private void bdot_Click(object sender, RoutedEventArgs e) => requaredMethods.NumberBtnClick(',');
+        private void bdivide_Click(object sender, RoutedEventArgs e) => requaredMethods.AddOperator(" / ");
+        private void bmultiply_Click(object sender, RoutedEventArgs e) => requaredMethods.AddOperator(" * ");
+        private void bsubtract_Click(object sender, RoutedEventArgs e) => requaredMethods.AddOperator(" - ");
+        private void bplus_Click(object sender, RoutedEventArgs e) => requaredMethods.AddOperator(" + ");
+        private void bpercentagedivide_Click(object sender, RoutedEventArgs e) => requaredMethods.AddOperator(" % ");
+        private void bclear_Click(object sender, RoutedEventArgs e) => requaredMethods.Clear();
+        private void bequal_Click(object sender, RoutedEventArgs e) => requaredMethods.Calculate();
+    }
+}
---
+++ WpfCalculatorApp/RequaredMethods.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Linq;
+
+namespace WpfCalculatorApp
+{
+    internal class RequaredMethods
+    {
+        public MainWindow MainWindow { get; set; }
+        private static MainWindow _mainWindow;
+
+        public RequaredMethods(MainWindow mainWindow)
+        {
+            _mainWindow = mainWindow;
+        }
+        public void NumberBtnClick(object element)
+        {
+            _mainWindow.tmpSaver = _mainWindow.tmpSaver.Equals("0") ? $"{element}" : _mainWindow.tmpSaver + element;
+
+            if (_mainWindow.tmpSaver.Length >= 11)
+                _mainWindow.MainInput2.Text = _mainWindow.tmpSaver.Substring(_mainWindow.tmpSaver.Length - 11);
+
+            else _mainWindow.MainInput2.Text = _mainWindow.tmpSaver;
+        }
+
+        public void AddOperator(string element)
+        {
+            if (!_mainWindow.tmpSaver.EndsWith(" "))
+                _mainWindow.tmpSaver += element;
+
+            if (_mainWindow.tmpSaver.Length >= 11)
+                _mainWindow.MainInput2.Text = _mainWindow.tmpSaver.Substring(_mainWindow.tmpSaver.Length - 11);
+
+            else _mainWindow.MainInput2.Text = _mainWindow.tmpSaver;
+        }
+
+        public void Clear()
+        {
+            if (_mainWindow.tmpSaver != "0")
+            {
+                if (!_mainWindow.tmpSaver.EndsWith(" "))
+                    _mainWindow.tmpSaver = _mainWindow.tmpSaver.Remove(_mainWindow.tmpSaver.Length - 1, 1).Length != 0
+                        ? _mainWindow.tmpSaver.Remove(_mainWindow.tmpSaver.Length - 1, 1)
+                        : "0";
+
+                else if (_mainWindow.tmpSaver[_mainWindow.tmpSaver.Length - 1] == ' ' && _mainWindow.tmpSaver[_mainWindow.tmpSaver.Length - 3] == ' ')
+                    _mainWindow.tmpSaver = _mainWindow.tmpSaver.Remove(_mainWindow.tmpSaver.Length - 3, 3);
+
+                if (_mainWindow.tmpSaver.Length >= 11)
+                    _mainWindow.MainInput2.Text = _mainWindow.tmpSaver.Substring(_mainWindow.tmpSaver.Length - 11);
+
+                else _mainWindow.MainInput2.Text = _mainWindow.tmpSaver;
+            }
+        }
+
+        public void Calculate()
+        {
+            string[] allElements = _mainWindow.tmpSaver.Split(' ');
+            decimal result = decimal.Parse(allElements[0]);
+
+            if (allElements.Contains("/") || allElements.Contains("*") || allElements.Contains("%"))
+            {
+                int i = 2;
+                while (i < allElements.Length)
+                {
+                    if (allElements[i - 1] == "*" || allElements[i - 1] == "/" || allElements[i - 1] == "%")
+                    {
+                        switch (allElements[i - 1])
+                        {
+                            case "*":
+                                allElements[i - 2] = (decimal.Parse(allElements[i - 2]) * decimal.Parse(allElements[i])).ToString();
+                                break;
+
+                            case "/":
+                                if (allElements[i] == "0")
+                                {
+                                    _mainWindow.MainInput2.Text = "ERROR";
+                                    _mainWindow.tmpSaver = "0";
+                                    return;
+                                }
+                                allElements[i - 2] = (decimal.Parse(allElements[i - 2]) / decimal.Parse(allElements[i])).ToString();
+                                break;
+
+                            case "%":
+                                allElements[i - 2] = (decimal.Parse(allElements[i - 2]) % decimal.Parse(allElements[i])).ToString();
+                                break;
+
+                            default: break;
+                        }
+
+                        for (int j = i + 1; j < allElements.Length; j++)
+                        {
+                            allElements[j - 2] = allElements[j];
+                        }
+
+                        Array.Resize(ref allElements, allElements.Length - 2);
+                    }
+                    else i += 2;
+                }
+            }
+
+            for (int i = 2; i < allElements.Length; i += 2)
+            {
+                switch (allElements[i - 1])
+                {
+                    case "+":
+                        result += decimal.Parse(allElements[i]);
+                        break;
+
+                    case "-":
+                        result -= decimal.Parse(allElements[i]);
+                        break;
+
+                    default: break;
+                }
+            }
+            _mainWindow.MainInput1.Text = _mainWindow.tmpSaver;
+            _mainWindow.MainInput2.Text = result.ToString();
+            _mainWindow.tmpSaver = result.ToString();
+        }
+    }
+}
