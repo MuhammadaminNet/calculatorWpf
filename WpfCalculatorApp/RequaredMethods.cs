8cc8ab322a523a90433ef92f1e6dd0b1f7c76673
MuhammadaminNet <mburanbayevdev@gmail.com>
2023-05-02 16:32:46 +0500

    Fixing some bugs

--- WpfCalculatorApp/RequaredMethods.cs
+++ WpfCalculatorApp/RequaredMethods.cs
@@ -9,17 +9,15 @@ namespace WpfCalculatorApp
         private static MainWindow _mainWindow;
 
         public RequaredMethods(MainWindow mainWindow)
-        {
-            _mainWindow = mainWindow;
-        }
+            => _mainWindow = mainWindow;
+
         public void NumberBtnClick(object element)
         {
             _mainWindow.tmpSaver = _mainWindow.tmpSaver.Equals("0") ? $"{element}" : _mainWindow.tmpSaver + element;
 
-            if (_mainWindow.tmpSaver.Length >= 11)
-                _mainWindow.MainInput2.Text = _mainWindow.tmpSaver.Substring(_mainWindow.tmpSaver.Length - 11);
-
-            else _mainWindow.MainInput2.Text = _mainWindow.tmpSaver;
+            _mainWindow.MainInput2.Text = _mainWindow.tmpSaver.Length >= 11 
+                ? _mainWindow.tmpSaver.Substring(_mainWindow.tmpSaver.Length - 11)
+                : _mainWindow.tmpSaver;
         }
 
         public void AddOperator(string element)
@@ -27,10 +25,9 @@ namespace WpfCalculatorApp
             if (!_mainWindow.tmpSaver.EndsWith(" "))
                 _mainWindow.tmpSaver += element;
 
-            if (_mainWindow.tmpSaver.Length >= 11)
-                _mainWindow.MainInput2.Text = _mainWindow.tmpSaver.Substring(_mainWindow.tmpSaver.Length - 11);
-
-            else _mainWindow.MainInput2.Text = _mainWindow.tmpSaver;
+            _mainWindow.MainInput2.Text = _mainWindow.tmpSaver.Length >= 11
+                 ? _mainWindow.tmpSaver.Substring(_mainWindow.tmpSaver.Length - 11)
+                 : _mainWindow.MainInput2.Text = _mainWindow.tmpSaver;
         }
 
         public void Clear()
@@ -42,13 +39,13 @@ namespace WpfCalculatorApp
                         ? _mainWindow.tmpSaver.Remove(_mainWindow.tmpSaver.Length - 1, 1)
                         : "0";
 
-                else if (_mainWindow.tmpSaver[_mainWindow.tmpSaver.Length - 1] == ' ' && _mainWindow.tmpSaver[_mainWindow.tmpSaver.Length - 3] == ' ')
-                    _mainWindow.tmpSaver = _mainWindow.tmpSaver.Remove(_mainWindow.tmpSaver.Length - 3, 3);
+                else if (_mainWindow.tmpSaver[_mainWindow.tmpSaver.Length - 1] == ' ' && 
+                            _mainWindow.tmpSaver[_mainWindow.tmpSaver.Length - 3] == ' ')
+                                _mainWindow.tmpSaver = _mainWindow.tmpSaver.Remove(_mainWindow.tmpSaver.Length - 3, 3);
 
-                if (_mainWindow.tmpSaver.Length >= 11)
-                    _mainWindow.MainInput2.Text = _mainWindow.tmpSaver.Substring(_mainWindow.tmpSaver.Length - 11);
-
-                else _mainWindow.MainInput2.Text = _mainWindow.tmpSaver;
+                _mainWindow.MainInput2.Text = _mainWindow.tmpSaver.Length >= 11
+                 ? _mainWindow.tmpSaver.Substring(_mainWindow.tmpSaver.Length - 11)
+                 : _mainWindow.tmpSaver;
             }
         }
 
@@ -57,7 +54,51 @@ namespace WpfCalculatorApp
             string[] allElements = _mainWindow.tmpSaver.Split(' ');
             decimal result = decimal.Parse(allElements[0]);
 
-            if (allElements.Contains("/") || allElements.Contains("*") || allElements.Contains("%"))
+            if (!allElements.Contains("/") && !allElements.Contains("*") && !allElements.Contains("%"))
+            {
+                for (int i = 2; i < allElements.Length; i += 2)
+                {
+                    switch (allElements[i - 1])
+                    {
+                        case "+":
+                            result += decimal.Parse(allElements[i]);
+                                break;
+
+                        case "-":
+                            result -= decimal.Parse(allElements[i]);
+                                break;
+
+                        default: 
+                            break;
+                    }
+                }
+            }
+
+            else if (!(allElements.Contains("-") && allElements.Contains("+")))
+            {
+                for (int i = 2; i < allElements.Length; i += 2)
+                {
+                    switch (allElements[i - 1])
+                    {
+                        case "/":
+                            result /= decimal.Parse(allElements[i]);
+                                break;
+
+                        case "*":
+                            result *= decimal.Parse(allElements[i]);
+                                break;
+
+                        case "%":
+                            result %= decimal.Parse(allElements[i]);
+                                break;
+
+                        default: 
+                            break;
+                    }
+                }
+            }
+
+            else 
             {
                 int i = 2;
                 while (i < allElements.Length)
@@ -84,7 +125,8 @@ namespace WpfCalculatorApp
                                 allElements[i - 2] = (decimal.Parse(allElements[i - 2]) % decimal.Parse(allElements[i])).ToString();
                                 break;
 
-                            default: break;
+                            default: 
+                                break;
                         }
 
                         for (int j = i + 1; j < allElements.Length; j++)
@@ -96,23 +138,24 @@ namespace WpfCalculatorApp
                     }
                     else i += 2;
                 }
-            }
 
-            for (int i = 2; i < allElements.Length; i += 2)
-            {
-                switch (allElements[i - 1])
+                for (i = 2; i < allElements.Length; i += 2)
                 {
-                    case "+":
-                        result += decimal.Parse(allElements[i]);
-                        break;
+                    switch (allElements[i - 1])
+                    {
+                        case "+":
+                            result += decimal.Parse(allElements[i]);
+                                break;
 
-                    case "-":
-                        result -= decimal.Parse(allElements[i]);
-                        break;
+                        case "-":
+                            result -= decimal.Parse(allElements[i]);
+                                break;
 
-                    default: break;
+                        default: break;
+                    }
                 }
             }
+
             _mainWindow.MainInput1.Text = _mainWindow.tmpSaver;
             _mainWindow.MainInput2.Text = result.ToString();
             _mainWindow.tmpSaver = result.ToString();
